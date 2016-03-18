1ef9fef71afaf82e03e2c30826dc77dea6c01e9f
winishasmith <winishasmith@gmail.com>
2016-03-18 16:23:43 -0400

    update tasks controller defs and added all html.erb files

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,14 +1,24 @@
 class TasksController < ApplicationController
+  before_action :find_task, only: [:show, :edit, :update, :destroy]
   def index
-  end
-
-  def show
+    @task = Task.all
   end
 
   def new
+    @task = Task.new
   end
 
   def create
+    @task = Task.new(task_params)
+
+    if @task.save
+      redirect_to @task
+    else
+      render 'new'
+    end
+  end
+
+  def show
   end
 
   def edit
@@ -21,8 +31,12 @@ class TasksController < ApplicationController
   end
 
   private
-  
+
+  def find_task
+    @task = Task.find(params[:id])
+  end
   def task_params
+    params.require(:task).permit(:title, :content)
   end
 
 end
