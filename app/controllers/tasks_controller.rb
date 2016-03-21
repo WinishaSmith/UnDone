649ebe1ba543dfb34606c7b1e525e1113e83eb87
winishasmith <winishasmith@gmail.com>
2016-03-20 23:05:43 -0400

    added tasks to index page

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,7 @@
 class TasksController < ApplicationController
   before_action :find_task, only: [:show, :edit, :update, :destroy]
   def index
-    @task = Task.all
+    @tasks = Task.all
   end
 
   def new
