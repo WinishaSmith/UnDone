265284fdb154e32215ba4e55b19d40036f9e1112
winishasmith <winishasmith@gmail.com>
2016-03-22 01:19:47 -0400

    updated routes and created html files for users

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
   root "tasks#index"
-  resources :users do
-    resources :tasks do
-      member do
-        get :complete
-      end
+  resources :users
+  resources :tasks do
+    member do
+      get :complete
     end
   end
 end
