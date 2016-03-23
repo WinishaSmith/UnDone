699e091bc878e00963a9781b5287fe12d5684e1d
winishasmith <winishasmith@gmail.com>
2016-03-23 11:18:00 -0400

    redirect users to task list on sign in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,7 +10,7 @@ class SessionsController < ApplicationController
       if @user.password === params[:user][:password]
         puts "You're signed in!"
         session[:user_id] = @user.id
-        redirect_to root_path
+        redirect_to user_tasks_path(@user)
       else
         puts "Wrong password!"
         redirect_to new_session_path
