3494a6c8aedcf4f3630209cd036cfc665a991003
winishasmith <winishasmith@gmail.com>
2016-04-27 18:44:13 -0400

    updated routes and added ability to create new list

--- app/controllers/task_lists_controller.rb
+++ app/controllers/task_lists_controller.rb
@@ -4,9 +4,35 @@ class TaskListsController < ApplicationController
     @task_lists = @user.task_lists
   end
 
+  def new
+     @user = User.find(params[:user_id])
+    @task_list = TaskList.new
+
+  end
+
   def create
     @user = User.find(params[:user_id])
-    @task_list = TaskList.new
+    @task_list = @user.task_lists.create!(task_list_params)
+
+    if @task_list.save
+      redirect_to user_task_lists_path(@user)
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @task_list = TaskList.find(params[:id])
+  end
+
+  private
+
+  def find_task_list
+    @task_list = TaskList.find(params[:id])
+  end
+
+  def task_list_params
+    params.require(:task_list).permit(:title, :description)
   end
 
 end
