130efa38a31f9c2e530a672d068e7f6775d65dc4
winishasmith <winishasmith@gmail.com>
2016-05-22 20:55:26 -0400

    removed comment from task_list controller, fixed task model spacing

--- app/controllers/task_lists_controller.rb
+++ app/controllers/task_lists_controller.rb
@@ -40,7 +40,6 @@ class TaskListsController < ApplicationController
     end
   end
 
-# need to fix this, unable to delete list
   def destroy
     @task_list.destroy
     redirect_to user_task_lists_path, notice: 'Tasklist successfully deleted.'
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,7 +1,8 @@
 class Task < ActiveRecord::Base
   belongs_to :task_lists
+  
   def completed?
     !completed_at.blank?
   end
-  # Woo! First model method I've seen.
+
 end
